7b201ed0228c28963875d8d39f627e97555a65a8
clodomir vianna da silva neto <clovianna_begins@hotmail.com>
2024-02-20 01:39:59 -0300

    Version 1.0.0 finished.

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 setTimeout(() => {
     // Background gradual zoom effect activation.
     document.getElementById("dynamic-background").style.scale = 2;
-
-    // Logo rotation animation programming. 
-    logo_transition_time = 3000;
-    document.getElementById("logo").style.transform = "rotate3d(0,0,1,-20deg)";
+    // Fermi telescope figure gradual distancing effect.
+    document.getElementById("telescope").style.width = "10%";
+    // Telescope rotation animation programming. 
+    telescope_transition_time = 3000;
+    document.getElementById("telescope").style.transform = "rotate3d(0,0,1,-20deg)";
         setTimeout(() => {
-            document.getElementById("logo").style.transform = "rotate3d(0,0,1,20deg)";
-        }, logo_transition_time);
+            document.getElementById("telescope").style.transform = "rotate3d(0,0,1,20deg)";
+        }, telescope_transition_time);
     function myCallback() {
-        document.getElementById("logo").style.transform = "rotate3d(0,0,1,-20deg)";
+        document.getElementById("telescope").style.transform = "rotate3d(0,0,1,-20deg)";
         setTimeout(() => {
-            document.getElementById("logo").style.transform = "rotate3d(0,0,1,20deg)";
-        }, logo_transition_time);
+            document.getElementById("telescope").style.transform = "rotate3d(0,0,1,20deg)";
+        }, telescope_transition_time);
 
         console.log("test");
     }    
-    const intervalID = setInterval(myCallback, 2*logo_transition_time);
+    const intervalID = setInterval(myCallback, 2*telescope_transition_time);
 
 }, 500);
 
