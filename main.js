c8081982d592961f0f81663ce579c6fec99dc999
clodomir vianna da silva neto <clovianna_begins@hotmail.com>
2024-02-22 13:36:29 -0300

    Reactivated javascript-based animations.

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-// if (window.innerWidth > 1000){
-// setTimeout(() => {
-//     // Background gradual zoom effect activation.
-//     document.getElementById("dynamic-background").style.scale = 2;
-//     // Fermi telescope figure gradual distancing effect.
-//     document.getElementById("telescope").style.width = "10%";
-//     // Telescope rotation animation programming. 
-//     telescope_transition_time = 3000;
-//     document.getElementById("telescope").style.transform = "rotate3d(0,0,1,-20deg)";
-//         setTimeout(() => {
-//             document.getElementById("telescope").style.transform = "rotate3d(0,0,1,20deg)";
-//         }, telescope_transition_time);
-//     function myCallback() {
-//         document.getElementById("telescope").style.transform = "rotate3d(0,0,1,-20deg)";
-//         setTimeout(() => {
-//             document.getElementById("telescope").style.transform = "rotate3d(0,0,1,20deg)";
-//         }, telescope_transition_time);
+if (window.innerWidth > 1000){
+setTimeout(() => {
+    // Background gradual zoom effect activation.
+    document.getElementById("dynamic-background").style.scale = 2;
+    // Fermi telescope figure gradual distancing effect.
+    document.getElementById("telescope").style.width = "10%";
+    // Telescope rotation animation programming. 
+    telescope_transition_time = 3000;
+    document.getElementById("telescope").style.transform = "rotate3d(0,0,1,-20deg)";
+        setTimeout(() => {
+            document.getElementById("telescope").style.transform = "rotate3d(0,0,1,20deg)";
+        }, telescope_transition_time);
+    function myCallback() {
+        document.getElementById("telescope").style.transform = "rotate3d(0,0,1,-20deg)";
+        setTimeout(() => {
+            document.getElementById("telescope").style.transform = "rotate3d(0,0,1,20deg)";
+        }, telescope_transition_time);
 
-//         console.log("test");
-//     }    
-//     const intervalID = setInterval(myCallback, 2*telescope_transition_time);
+        console.log("test");
+    }    
+    const intervalID = setInterval(myCallback, 2*telescope_transition_time);
 
-// }, 500);
+}, 500);
 
-// }
+}
 
 function changeOSinstructions(instructionId, buttonid){
 
