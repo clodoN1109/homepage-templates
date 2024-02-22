7244fd96942fe88be741003d01d6f5b97ad657cd
clodomir vianna da silva neto <clovianna_begins@hotmail.com>
2024-02-22 13:15:38 -0300

    Fixed copy buttons logic.

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-if (window.innerWidth > 1000){
-setTimeout(() => {
-    // Background gradual zoom effect activation.
-    document.getElementById("dynamic-background").style.scale = 2;
-    // Fermi telescope figure gradual distancing effect.
-    document.getElementById("telescope").style.width = "10%";
-    // Telescope rotation animation programming. 
-    telescope_transition_time = 3000;
-    document.getElementById("telescope").style.transform = "rotate3d(0,0,1,-20deg)";
-        setTimeout(() => {
-            document.getElementById("telescope").style.transform = "rotate3d(0,0,1,20deg)";
-        }, telescope_transition_time);
-    function myCallback() {
-        document.getElementById("telescope").style.transform = "rotate3d(0,0,1,-20deg)";
-        setTimeout(() => {
-            document.getElementById("telescope").style.transform = "rotate3d(0,0,1,20deg)";
-        }, telescope_transition_time);
+// if (window.innerWidth > 1000){
+// setTimeout(() => {
+//     // Background gradual zoom effect activation.
+//     document.getElementById("dynamic-background").style.scale = 2;
+//     // Fermi telescope figure gradual distancing effect.
+//     document.getElementById("telescope").style.width = "10%";
+//     // Telescope rotation animation programming. 
+//     telescope_transition_time = 3000;
+//     document.getElementById("telescope").style.transform = "rotate3d(0,0,1,-20deg)";
+//         setTimeout(() => {
+//             document.getElementById("telescope").style.transform = "rotate3d(0,0,1,20deg)";
+//         }, telescope_transition_time);
+//     function myCallback() {
+//         document.getElementById("telescope").style.transform = "rotate3d(0,0,1,-20deg)";
+//         setTimeout(() => {
+//             document.getElementById("telescope").style.transform = "rotate3d(0,0,1,20deg)";
+//         }, telescope_transition_time);
 
-        console.log("test");
-    }    
-    const intervalID = setInterval(myCallback, 2*telescope_transition_time);
+//         console.log("test");
+//     }    
+//     const intervalID = setInterval(myCallback, 2*telescope_transition_time);
 
-}, 500);
+// }, 500);
 
-}
+// }
 
 function changeOSinstructions(instructionId, buttonid){
 
@@ -49,13 +49,19 @@ function changeOSinstructions(instructionId, buttonid){
 
 }
 
-function copyToClipboard() {
-    const codeSnippet = document.querySelector('.code-box code');
+function copyToClipboard(event) {
+
+    codeSnippet = event.target.previousElementSibling.textContent;
+
     const textArea = document.createElement('textarea');
-    textArea.value = codeSnippet.textContent;
+    textArea.value = codeSnippet;
+
     document.body.appendChild(textArea);
+
     textArea.select();
     document.execCommand('copy');
+    alert(`"${codeSnippet} " copied to the clipboard!`);
     document.body.removeChild(textArea);
-    alert('Code snippet copied to clipboard!');
+    
+
 }
